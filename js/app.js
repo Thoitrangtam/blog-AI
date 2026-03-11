623d34e5e989585eaa74faf573fdd7ee71e57ebb
Tam Nguyen <tamnguyen@gmail.com>
2026-03-11 09:55:47 +0700

    fix UI

--- js/app.js
+++ js/app.js
@@ -1,31 +1,32 @@
-async function loadTools() {
+async function loadTools(){
 
 const res = await fetch('/data/tools.json');
+
 const tools = await res.json();
 
-const container = document.getElementById('tools-grid');
+const grid = document.getElementById('tools-grid');
 
-tools.forEach(tool => {
+tools.forEach(tool=>{
 
 const domain = new URL(tool.link).hostname;
 
 const logo = `https://www.google.com/s2/favicons?domain=${domain}&sz=128`;
 
-const card = `
+grid.innerHTML += `
+
 <div class="tool-card">
 
-<img src="${logo}" alt="${tool.name}">
+<img src="${logo}">
 
 <h3>${tool.name}</h3>
 
 <p>${tool.description}</p>
 
-<a href="/tool.html?name=${tool.name}">View Tool</a>
+<a href="${tool.link}" target="_blank">Visit</a>
 
 </div>
-`;
 
-container.innerHTML += card;
+`;
 
 });
 
